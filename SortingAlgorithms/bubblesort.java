c95feb86c65b1a3629bbca7295f717d641e797f9
sriram200408 <f20221082@pilani.bits-pilani.ac.in>
2025-09-06 02:10:14 +0530

    sorting algorithms

--- SortingAlgorithms/bubblesort.java
+++ SortingAlgorithms/bubblesort.java
@@ -1,5 +1,38 @@
 package SortingAlgorithms;
 
-public class bubblesort {
+import java.util.Scanner;
 
+public class bubblesort {
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    static void bubbleSort(int[] arr) {
+        int idx = arr.length;
+        while (idx != 0) {
+            for (int i = 0; i < idx - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+            idx--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
 }
--- SortingAlgorithms/insertionsort.java
+++ SortingAlgorithms/insertionsort.java
@@ -1,5 +1,44 @@
 package SortingAlgorithms;
 
+import java.util.Scanner;
+
 public class insertionsort {
 
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[b];
+        arr[b] = arr[a];
+        arr[a] = temp;
+    }
+
+    static void InsertionSort(int[] arr) {
+        int idx = 1;
+        while (idx != arr.length) {
+            int i = idx - 1;
+            while (i > -1) {
+                if (arr[idx] < arr[i]) {
+                    swap(arr, idx, i);
+                    idx = i;
+
+                }
+                i--;
+            }
+
+            idx++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        InsertionSort(arr);
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
 }
