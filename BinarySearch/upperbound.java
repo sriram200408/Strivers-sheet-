9514b168df9793d1cff63bde07078d93f3a98633
sriram200408 <f20221082@pilani.bits-pilani.ac.in>
2026-06-29 20:53:58 +0530

    update

--- BinarySearch/upperbound.java
+++ BinarySearch/upperbound.java
@@ -2,19 +2,19 @@ package BinarySearch;
 
 import java.util.Scanner;
 
-public class lowerbound {
-    static int lower(int[] arr, int ele) {
+public class upperbound {
+    static int upper(int[] arr, int ele) {
         int start = 0;
         int end = arr.length - 1;
         int mid = (start + end) / 2;
         int ans = arr.length;
         while (start <= end) {
             mid = (start + end) / 2;
-            if (ele <= arr[mid]) {
+            if (ele < arr[mid]) {
                 ans = mid;
                 end = mid - 1;
 
-            } else if (ele > arr[mid]) {
+            } else if (ele >= arr[mid]) {
                 start = mid + 1;
             }
 
@@ -33,7 +33,7 @@ public class lowerbound {
         }
         System.out.println("enter value to search");
         int ele = sc.nextInt();
-        System.out.println("the index is " + lower(arr, ele));
+        System.out.println("the index is " + upper(arr, ele));
         sc.close();
     }
 }
