cdfb555ea81aedaca6a07abcc0d91cd0b7f10c0f
sriram200408 <f20221082@pilani.bits-pilani.ac.in>
2026-06-29 18:20:58 +0530

    update

---
+++ BinarySearch/lowerbound.java
@@ -0,0 +1,39 @@
+package BinarySearch;
+
+import java.util.Scanner;
+
+public class lowerbound {
+    static int lower(int[] arr, int ele) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = (start + end) / 2;
+        int ans = arr.length;
+        while (mid <= end) {
+            mid = (start + end) / 2;
+            if (ele <= arr[mid]) {
+                ans = mid;
+                end = mid - 1;
+
+            } else if (ele > arr[mid]) {
+                start = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter array values");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("enter value to search");
+        int ele = sc.nextInt();
+        System.out.println("the index is " + lower(arr, ele));
+        sc.close();
+    }
+}
